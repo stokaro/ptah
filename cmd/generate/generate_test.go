0b2f97307864b7c12bb93b5dc2a6958b3257861a
Denis Voytyuk <5462781+denisvmedia@users.noreply.github.com>
2026-07-17 00:19:10 +0200

    Add Atlas HCL schema input (#329)

--- cmd/generate/generate_test.go
+++ cmd/generate/generate_test.go
@@ -28,12 +28,34 @@ tables:
 	c.Assert(db.Tables[0].Name, qt.Equals, "users")
 }
 
-func TestLoadSchemaFile_RejectsUnsupportedExtension(t *testing.T) {
+func TestLoadSchemaFile_AtlasHCL(t *testing.T) {
 	c := qt.New(t)
 
 	path := filepath.Join(t.TempDir(), "schema.hcl")
-	c.Assert(os.WriteFile(path, []byte(`schema "users" {}`), 0o600), qt.IsNil)
+	c.Assert(os.WriteFile(path, []byte(`
+table "users" {
+  column "id" {
+    type = int
+  }
+  primary_key {
+    columns = [column.id]
+  }
+}
+`), 0o600), qt.IsNil)
+
+	db, err := loadSchema("", path)
+	c.Assert(err, qt.IsNil)
+	c.Assert(db.Tables, qt.HasLen, 1)
+	c.Assert(db.Tables[0].Name, qt.Equals, "users")
+	c.Assert(db.Fields[0].Primary, qt.IsTrue)
+}
+
+func TestLoadSchemaFile_RejectsUnsupportedExtension(t *testing.T) {
+	c := qt.New(t)
+
+	path := filepath.Join(t.TempDir(), "schema.json")
+	c.Assert(os.WriteFile(path, []byte(`{}`), 0o600), qt.IsNil)
 
 	_, err := loadSchema("", path)
-	c.Assert(err, qt.ErrorMatches, `unsupported schema file extension ".hcl": only .yaml and .yml are supported`)
+	c.Assert(err, qt.ErrorMatches, `unsupported schema file extension ".json": only .yaml, .yml, and .hcl are supported`)
 }
