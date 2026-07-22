e600f9fd175c3eb67da7348c0699901a96476f23
Denis V <5462781+denisvmedia@users.noreply.github.com>
2026-07-22 13:18:26 +0200

    Refactor Atlas schema fmt runtime

--- cmd/atlas/schema_fmt.go
+++ cmd/atlas/schema_fmt.go
@@ -1,16 +1,12 @@
 package atlas
 
 import (
-	"bytes"
 	"fmt"
-	"os"
-	"path/filepath"
 
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/spf13/cobra"
 
 	"github.com/stokaro/ptah/cmd/internal/cmdutil"
+	"github.com/stokaro/ptah/internal/atlashclfmt"
 )
 
 func newAtlasSchemaFmtCommand() *cobra.Command {
@@ -34,82 +30,12 @@ func runAtlasSchemaFmt(cmd *cobra.Command, args []string) error {
 		paths = []string{"."}
 	}
 
-	for _, path := range paths {
-		changed, err := formatAtlasSchemaPath(path)
-		if err != nil {
-			return err
-		}
-		for _, file := range changed {
-			fmt.Fprintln(cmd.OutOrStdout(), file)
-		}
+	changed, err := atlashclfmt.FormatPaths(paths)
+	if err != nil {
+		return err
+	}
+	for _, file := range changed {
+		fmt.Fprintln(cmd.OutOrStdout(), file)
 	}
 	return nil
-}
-
-func formatAtlasSchemaPath(path string) ([]string, error) {
-	info, err := os.Stat(path)
-	if err != nil {
-		return nil, fmt.Errorf("schema fmt %s: %w", path, err)
-	}
-	if !info.IsDir() {
-		changed, err := formatAtlasSchemaFile(path)
-		if err != nil || !changed {
-			return nil, err
-		}
-		return []string{path}, nil
-	}
-
-	var changed []string
-	err = filepath.WalkDir(path, func(file string, entry os.DirEntry, walkErr error) error {
-		if walkErr != nil {
-			return walkErr
-		}
-		if entry.IsDir() || filepath.Ext(file) != ".hcl" {
-			return nil
-		}
-		fileChanged, err := formatAtlasSchemaFile(file)
-		if err != nil {
-			return err
-		}
-		if fileChanged {
-			changed = append(changed, file)
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, fmt.Errorf("schema fmt %s: %w", path, err)
-	}
-	return changed, nil
-}
-
-func formatAtlasSchemaFile(path string) (bool, error) {
-	if filepath.Ext(path) != ".hcl" {
-		return false, nil
-	}
-
-	info, err := os.Stat(path)
-	if err != nil {
-		return false, fmt.Errorf("schema fmt %s: %w", path, err)
-	}
-	if info.IsDir() {
-		return false, nil
-	}
-
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return false, fmt.Errorf("schema fmt %s: %w", path, err)
-	}
-	if _, diags := hclwrite.ParseConfig(data, path, hcl.Pos{Line: 1, Column: 1}); diags.HasErrors() {
-		return false, fmt.Errorf("schema fmt %s: %s", path, diags.Error())
-	}
-
-	formatted := hclwrite.Format(data)
-	if bytes.Equal(data, formatted) {
-		return false, nil
-	}
-	//nolint:gosec // schema fmt intentionally rewrites the user-selected local HCL file after stat/read validation.
-	if err := os.WriteFile(path, formatted, info.Mode().Perm()); err != nil {
-		return false, fmt.Errorf("schema fmt %s: %w", path, err)
-	}
-	return true, nil
 }
