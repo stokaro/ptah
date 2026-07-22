827bfcdcc7ec20e330b221f4baa3cf9d64080f74
Denis V <5462781+denisvmedia@users.noreply.github.com>
2026-07-22 11:30:37 +0200

    Move Atlas format rendering out of cmd

--- internal/atlasreport/schema_diff_test.go
+++ internal/atlasreport/schema_diff_test.go
@@ -1,18 +1,20 @@
-package atlasreport
+package atlasreport_test
 
 import (
 	"bytes"
 	"testing"
 
 	qt "github.com/frankban/quicktest"
+
+	"github.com/stokaro/ptah/internal/atlasreport"
 )
 
 func TestSchemaDiffDefaultFormatReportsSyncedSchemas(t *testing.T) {
 	c := qt.New(t)
 	var out bytes.Buffer
-	report := NewSchemaDiff(nil, nil, nil)
+	report := atlasreport.NewSchemaDiff(nil, nil, nil)
 
-	err := WriteSchemaDiff(&out, NormalizeSchemaDiffFormat(""), report)
+	err := atlasreport.WriteSchemaDiff(&out, atlasreport.NormalizeSchemaDiffFormat(""), report)
 
 	c.Assert(err, qt.IsNil)
 	c.Assert(out.String(), qt.Equals, "Schemas are synced, no changes to be made.\n")
@@ -21,11 +23,11 @@ func TestSchemaDiffDefaultFormatReportsSyncedSchemas(t *testing.T) {
 func TestSchemaDiffCustomSQLTemplate(t *testing.T) {
 	c := qt.New(t)
 	var out bytes.Buffer
-	report := NewSchemaDiff(nil, nil, []string{
+	report := atlasreport.NewSchemaDiff(nil, nil, []string{
 		`CREATE TABLE "users" ("id" integer);`,
 	})
 
-	err := WriteSchemaDiff(&out, `{{ len .Changes }}|{{ .MarshalSQL }}|{{ sql . "  " }}`, report)
+	err := atlasreport.WriteSchemaDiff(&out, `{{ len .Changes }}|{{ .MarshalSQL }}|{{ sql . "  " }}`, report)
 
 	c.Assert(err, qt.IsNil)
 	c.Assert(out.String(), qt.Equals, "1|CREATE TABLE \"users\" (\"id\" integer);\n|  CREATE TABLE \"users\" (\"id\" integer);\n")
@@ -34,7 +36,7 @@ func TestSchemaDiffCustomSQLTemplate(t *testing.T) {
 func TestSchemaDiffTemplateValidationRejectsUnknownHelpers(t *testing.T) {
 	c := qt.New(t)
 
-	err := ValidateSchemaDiffTemplate(`{{ json . }}`)
+	err := atlasreport.ValidateSchemaDiffTemplate(`{{ json . }}`)
 
 	c.Assert(err, qt.ErrorMatches, `parse --format template: .*function "json" not defined.*`)
 }
@@ -42,11 +44,11 @@ func TestSchemaDiffTemplateValidationRejectsUnknownHelpers(t *testing.T) {
 func TestSchemaDiffTemplateExecutionErrorDoesNotWritePartialOutput(t *testing.T) {
 	c := qt.New(t)
 	var out bytes.Buffer
-	report := NewSchemaDiff(nil, nil, []string{
+	report := atlasreport.NewSchemaDiff(nil, nil, []string{
 		`CREATE TABLE "users" ("id" integer);`,
 	})
 
-	err := WriteSchemaDiff(&out, `before {{ sql . "  " "extra" }}`, report)
+	err := atlasreport.WriteSchemaDiff(&out, `before {{ sql . "  " "extra" }}`, report)
 
 	c.Assert(err, qt.ErrorMatches, `execute --format template: .*unexpected number of arguments: 2.*`)
 	c.Assert(out.String(), qt.Equals, "")
